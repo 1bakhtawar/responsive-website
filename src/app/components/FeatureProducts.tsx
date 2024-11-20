a1f18d4aecc67614355498eb0b600dd4159c540d
Bakhtawar <bakhtawar@codewithbakhtawar.com>
2024-11-20 20:19:09 +0500

    first commit

--- src/app/components/FeatureProducts.tsx
+++ src/app/components/FeatureProducts.tsx
@@ -7,16 +7,16 @@ const FeatureProducts = () => {
             <div className='md:flex gap-4 md:mb-32'>
         <div className='w-full'>
         <Image src="/img1.jpg" alt="image" width={200} height={200} className="mb-2 sm:h-9 mx-auto md:w-full md:h-full rounded-xl hover:scale-75" />
-         <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 10,000</h1>
+         <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 5,000</h1>
             <div className='sm:h-12 ml-20 w-24 text-sm text-center mb-6 rounded-xl bg-blue-900 md:w-32 text-white md:text-lg md:h-10 md:mt-4 md:ml-5'>
             <button className='text-center md:ml-7 md:mt-1 md:mr-5'>Buy Now</button>
             </div>
        </div>
-       
+
         <div className='w-full'>
         <Image src="/img2.jpg" alt="image" width={200} height={200} className="mb-2 sm:h-9 mx-auto md:w-full md:h-full rounded-xl hover:scale-75" />
         <div>
-        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 10,000</h1>
+        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 3,000</h1>
         <div className='sm:h-12 ml-20 w-24 text-sm text-center mb-6 rounded-xl bg-blue-900 md:w-32 text-white md:text-lg md:h-10 md:mt-4 md:ml-5'>
             <button className='text-center md:ml-7 md:mt-1 md:mr-5'>Buy Now</button>
            </div>
@@ -26,7 +26,7 @@ const FeatureProducts = () => {
         <div className='w-full'>
         <Image src="/img3.jpg" alt="image" width={200} height={200} className="mb-2 sm:h-9 mx-auto md:w-full md:h-full rounded-xl hover:scale-75" />
        <div>
-        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 10,000</h1>
+        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 7,000</h1>
         <div className='sm:h-12 ml-20 w-24 text-sm text-center mb-6 rounded-xl bg-blue-900 md:w-32 text-white md:text-lg md:h-10 md:mt-4 md:ml-5'>
             <button className='text-center md:ml-7 md:mt-1 md:mr-5'>Buy Now</button>
             </div>
@@ -39,7 +39,7 @@ const FeatureProducts = () => {
         <Image src="/img1.jpg" alt="image" width={200} height={200} className="mx-auto sm:h-9 rounded-xl md:w-full md:h-full hover:scale-75" />
 
         <div>
-        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 10,000</h1>
+        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 5,000</h1>
         <div className='sm:h-12 ml-20 w-24 text-sm text-center mb-6 rounded-xl bg-blue-900 md:w-32 text-white md:text-lg md:h-10 md:mt-4 md:ml-5'>
             <button className='text-center md:ml-7 md:mt-1 md:mr-5'>Buy Now</button>
            </div>
@@ -49,7 +49,7 @@ const FeatureProducts = () => {
         <div className='w-full'>
         <Image src="/img2.jpg" alt="image" width={200} height={200} className="sm:w-9 mb-2 sm:h-9 mx-auto md:w-full md:h-full rounded-xl hover:scale-75" />
         <div>
-        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 10,000</h1>
+        <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 3,000</h1>
         <div className='sm:h-12 ml-20 w-24 text-sm text-center mb-6 rounded-xl bg-blue-900 md:w-32 text-white md:text-lg md:h-10 md:mt-4 md:ml-5'>
             <button className='text-center md:ml-7 md:mt-1 md:mr-5'>Buy Now</button>
           </div>
@@ -59,7 +59,7 @@ const FeatureProducts = () => {
         <div className='w-full'>
           <Image src="/img3.jpg" alt="image" width={200} height={200} className="md:left-0 md:top-0 sm:w-9 mb-2 sm:h-9 mx-auto md:w-full md:h-full rounded-xl hover:scale-75" />
         <div>
-          <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 10,000</h1>
+          <h1 className='md:mt-5 md:ml-5 ml-20'>PKR 7,000</h1>
           <div className='sm:h-12 ml-20 w-24 text-sm text-center mb-6 rounded-xl bg-blue-900 md:w-32 text-white md:text-lg md:h-10 md:mt-4 md:ml-5'>
             <button className='text-center md:ml-7 md:mt-1 md:mr-5'>Buy Now</button>
             </div>
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,7 +2,7 @@ const NavBar = () => {
   return (
     <div>
         <div className='w-full bg-blue-800 md:h-14 h-10  pt-0'>
-            <h1 className='text-center text-white text-sm pt-1 md:text-yellow-200 md:h-24 md:text-3xl lg:text-5xl md:font-bold md:font-serif'>Welcome To My Responsive Website</h1>
+            <h1 className='text-center text-white text-sm pt-1 md:text-yellow-200 md:h-24 md:text-3xl lg:text-5xl md:font-bold md:font-serif'>Welcome to my cosmetic shop</h1>
         </div>
       
     </div>
